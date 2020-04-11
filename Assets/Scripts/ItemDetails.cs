b9072ad852ae1e73fa377b8ab1ee79a85ec67c16
Jayasurya Aasaithambi <jayasurya00098@gmail.com>
2020-04-11 19:05:23 +0530

    Dynamic-Scrollable-List
    Init Commit

---
+++ Assets/Scripts/ItemDetails.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class ItemDetails
+{
+    //Item Name
+    public string name ;
+    //Item Image
+    public Sprite sprite;
+}
---
+++ Assets/Scripts/ListCreator.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ListCreator : MonoBehaviour
+{
+    public Transform SpawnPoint = null;
+    [Space]
+    public GameObject listItem = null;
+    [Space]
+    public RectTransform content = null;
+    [Space]
+    public ItemDetails[] itemDetails;
+    [Space]
+    public float itemHeight;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        //setContent Holder Height;
+        content.sizeDelta = new Vector2(0, itemDetails.Length * itemHeight);
+
+        foreach(ItemDetails item in itemDetails)
+        {
+            //newSpawn Position
+            Vector3 pos = new Vector3(SpawnPoint.position.x, -itemHeight, SpawnPoint.position.z);
+            //instantiate item
+            GameObject SpawnedItem = Instantiate(listItem, pos, SpawnPoint.rotation);
+            //setParent
+            SpawnedItem.transform.SetParent(SpawnPoint, false);
+            //set name
+            SpawnedItem.transform.GetChild(0).GetComponent<Text>().text = item.name;
+            //set image
+            SpawnedItem.transform.GetChild(1).GetComponent<Image>().sprite = item.sprite;
+        }
+    }
+}
